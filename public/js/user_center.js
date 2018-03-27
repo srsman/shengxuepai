c260831d5122e7cb6481cc01f421d22d8b78845e
gscsdlz <lz842063523@foxmail.com>
2018-03-27 16:59:12 +0800

    完成省市县联动操作

---
+++ public/js/user_center.js
@@ -0,0 +1,60 @@
+var citylist;
+var _provinceID = 0;
+var _cityId = 0;
+
+$(document).ready(function () {
+
+    $.get(URL + "/js/city.min.js", function(response){
+        citylist = eval( "(" + response + ")");
+        refresh_province();
+    });
+
+    $("#province").change(function(){
+        _provinceID = $(this).val();
+        refresh_city();
+    })
+
+    $("#city").change(function(){
+        _cityId = $(this).val();
+        refresh_country();
+    })
+
+})
+
+function  refresh_province() {
+    var str = '';
+    for (var i = 0; i < citylist.length; i++) {
+       str += '<option value=' + i+ '>' + citylist[i].p +'</option>'
+    }
+    $("#province").html(str);
+    refresh_city();
+}
+
+function refresh_city() {
+    var str = '';
+    if (typeof citylist[_provinceID].c == 'undefined') {
+        $("#city").hide();
+    } else {
+        var len = citylist[_provinceID].c.length;
+        for (var i = 0; i < len; i++) {
+            str += '<option value=' + i+ '>' + citylist[_provinceID].c[i].n +'</option>'
+        }
+        $("#city").html(str);
+        $("#city").show();
+    }
+    refresh_country();
+}
+
+function refresh_country() {
+    var str = '';
+    if (typeof citylist[_provinceID].c[_cityId].a == 'undefined') {
+        $("#country").hide();
+    } else {
+        var len = citylist[_provinceID].c[_cityId].a.length;
+        for (var i = 0; i < len; i++) {
+            str += '<option value=' + i+ '>' + citylist[_provinceID].c[_cityId].a[i].s +'</option>'
+        }
+        $("#country").html(str);
+        $("#country").show();
+    }
+}
