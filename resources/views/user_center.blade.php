07d054f08355afee4f1b9431e48160de9df1b90a
gscsdlz <lz842063523@foxmail.com>
2018-03-27 16:59:57 +0800

    用户中心

---
+++ resources/views/user_center.blade.php
@@ -0,0 +1,112 @@
+@extends('layout')
+@section('main')
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2"  style="padding: 20px 0px">
+            <div class="row">
+                <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 box">
+                    <p class="box-title">个人基本信息</p>
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">所在省市县</label>
+                            <div class="col-sm-3">
+                                <select class="form-control" id="province"></select>
+                            </div>
+                            <div class="col-sm-3">
+                                <select class="form-control" id="city" style="display: none"></select>
+                            </div>
+                            <div class="col-sm-3">
+                                <select class="form-control" id="country" style="display: none"></select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">就读班级</label>
+                            <div class="col-sm-3">
+                                <select type="text" class="form-control">
+                                    <option>高一</option>
+                                    <option>高二</option>
+                                    <option>高三</option>
+                                </select>
+                            </div>
+                            <div class="col-sm-3">
+                                <input type="text" class="form-control" value=""/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">就读中学</label>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" value=""/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">考生姓名</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="form-control" value=""/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">考生性别</label>
+                            <div class="col-sm-4">
+                                <label class="radio-inline">
+                                    <input type="radio" name="sex" value="option1"> 男
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="sex" value="option2"> 女
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">考生类型</label>
+                            <div class="col-sm-4">
+                                <label class="radio-inline">
+                                    <input type="radio" name="major" value="option1"> 理科
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="major" value="option2"> 文科
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">高考年份</label>
+                            <div class="col-sm-3">
+                                <select class="form-control">
+                                    @for($i = date('Y'); $i <= date('Y') + 4; $i++)
+                                        <option value="{{ $i }}">{{ $i }}年</option>
+                                    @endfor
+                                </select>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <hr/>
+            <div class="row">
+                <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 box">
+                    <p class="box-title">高考分数管理</p>
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">高考分数</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="form-control" value=""/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">高考位次</label>
+                            <div class="col-sm-3">
+                                <input type="text" disabled class="form-control" value=""/>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <hr/>
+            <p class="text-center"><button class="btn btn-sxp" type="button">确认修改</button></p>
+        </div>
+    </div>
+@endsection
+@section('script')
+    <script src="{{ URL::asset('/js/user_center.js') }}" charset="utf-8"></script>
+@endsection
