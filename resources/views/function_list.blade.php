614a8a87766f4ed36e5d714acf2bc32b95eca00d
gscsdlz <lz842063523@foxmail.com>
2018-03-27 15:03:01 +0800

    完成功能区域的其他样式

---
+++ resources/views/function_list.blade.php
@@ -0,0 +1,62 @@
+@extends('layout')
+@section('main')
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="row">
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/1.png') }}" />
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/2.png') }}" />
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/3.png') }}" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/4.png') }}" />
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/5.png') }}" />
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/6.png') }}" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/7.png') }}" />
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/8.png') }}" />
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/9.png') }}" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/10.png') }}" />
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/11.png') }}" />
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/12.png') }}" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/13.png') }}" />
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/14.png') }}" />
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-up" width="100%" src="{{ URL::asset('images/project/functions/15.png') }}" />
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
