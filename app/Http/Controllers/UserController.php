34875fd0090fb03919350075093e3efba2165727
gscsdlz <lz842063523@foxmail.com>
2018-03-28 16:10:21 +0800

    完成登录

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,10 @@
 namespace App\Http\Controllers;
 
 
+use App\Model\AccountModel;
+use App\Model\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
@@ -18,9 +21,66 @@ class UserController extends Controller
         $username = $request->get('username');
         $password = $request->get('password');
 
+        $account = AccountModel::select('user_id', 'pass', 'user_type', 'vip')
+            ->where('user', $username)
+            ->orWhere('phone', $username)->first();
+        if (!is_null($account)) {
+            if (md5($password) == $account->pass) {
+
+                $user = UserModel::select('name', 'sex', 'classify', 'school', 'rank_arg', 'year')
+                    ->where('user_id', $account->user_id)->first();
+
+                Session::put('user_id', $account->user_id);
+                Session::put('username', $account->username);
+                Session::put('user_type', $account->user_type);
+                Session::put('vip', $account->vip);
+
+                if (!is_null($user)) { //激活用户
+
+                    Session::put('name', $user->name);
+                    Session::put('sex', $user->sex == 1 ? '男' : '女');
+                    Session::put('school', $user->school);
+                    Session::put('year', $user->year);
+                    Session::put('classify', $user->classify == 1 ? '文科' : '理科');
+                    $rank = json_decode($user->rank_arg, true);
+                    if(isset($rank['rank']))
+                        $rank = $rank['rank'];
+                    else
+                        $rank = '未知';
+                    Session::put('rank',$rank);
+                }
+                return response()->json([
+                    'status' => true
+                ]);
+            }
+        }
         return response()->json([
-           'status' => false,
-           'info' => '数据库尚未准备好'
+            'status' => false,
+            'info' => '用户名或密码错误'
         ]);
+
+    }
+
+    public function test()
+    {
+        $res = UserModel::select('user_id', 'score', 'score_1','score_2','score_3', 'ranking', 'ranking_1')->get();
+        foreach ($res as $row) {
+            $arg1 = json_encode([
+                'score' => $row->score,
+                'score_1' => $row->score_1,
+                'score_2' => $row->score_2,
+                'score_3' => $row->Score_3
+            ]);
+            $arg2 = json_encode([
+               'rank' => $row->ranking,
+               'rank_1' => $row->ranking_1
+            ]);
+            UserModel::where('user_id', $row->user_id)
+                ->update([
+                    'score_arg' => $arg1,
+                    'rank_arg' => $arg2,
+                ]);
+        }
+        echo "ok";
     }
 }
