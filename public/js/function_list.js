eeda81768d5b5e1f3f7d06885588df724b95dfd5
gscsdlz <lz842063523@foxmail.com>
2018-03-28 16:08:03 +0800

    取消延时效果

--- public/js/function_list.js
+++ public/js/function_list.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
     $(".img-up").mouseover(function () {
         var that = $(this);
-        window.setTimeout(function(){
-            $("#zoom").hide();
-            $("#zoom").attr('src', $(that).attr('src'));
-            $("#zoom").css('top', $(that).offset().top - 100);
-            $("#zoom").css('left', $(that).offset().left - 400);
-            $("#zoom").fadeIn();
-        }, 600);
+        $("#zoom").hide();
+        $("#zoom").attr('src', $(that).attr('src'));
+        $("#zoom").css('top', $(that).offset().top - 100);
+        $("#zoom").css('left', $(that).offset().left - 400);
+        $("#zoom").show();
     })
 
     $(".img-up").mouseout(function () {
