1370e51cdf0d706d6fefcde6b1417ba3b79d6f64
gscsdlz <lz842063523@foxmail.com>
2018-03-29 18:01:51 +0800

    优化显示效果

--- public/js/function_list.js
+++ public/js/function_list.js
@@ -4,7 +4,10 @@ $(document).ready(function () {
         $("#zoom").hide();
         $("#zoom").attr('src', $(that).attr('src'));
         $("#zoom").css('top', $(that).offset().top - 100);
-        $("#zoom").css('left', $(that).offset().left - 400);
+        if ($(that).offset().left > document.documentElement.clientWidth / 2)
+            $("#zoom").css('left', $(that).offset().left + $(that).width());
+        else
+            $("#zoom").css('left', $(that).offset().left - 400);
         $("#zoom").show();
     })
 
