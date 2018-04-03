72722a95e6731c35e422fe1236cd15148d054e06
gscsdlz <lz842063523@foxmail.com>
2018-04-03 17:23:37 +0800

    加注释

--- public/js/app.js
+++ public/js/app.js
@@ -6,20 +6,20 @@ $(document).ready(function(){
     $(["[data-toggle='tooltip']"]).tooltip();
 
     $(".custom-checkbox").mouseover(function () {
-        if($(this).css('background-position') != '-10px -218px')
-            $(this).css('background-position', '-10px -118px');
+        if($(this).css('background-position') != '-10px -218px')   //选中
+            $(this).css('background-position', '-10px -118px');   //hover
     })
 
     $(".custom-checkbox").mouseout(function () {
-        if($(this).css('background-position') != '-10px -218px')
-            $(this).css('background-position', '-10px -18px');
+        if($(this).css('background-position') != '-10px -218px')   //选中
+            $(this).css('background-position', '-10px -18px');    //初始
     })
 
     $(".custom-checkbox").click(function () {
-        if($(this).css('background-position') == '-10px -118px') {
-            $(this).css('background-position', '-10px -218px');
+        if($(this).css('background-position') == '-10px -118px') {  //hover
+            $(this).css('background-position', '-10px -218px');    //选中
         } else {
-            $(this).css('background-position', '-10px -118px');
+            $(this).css('background-position', '-10px -118px');    //hover
         }
     })
 })
