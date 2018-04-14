5f8b1fbeeefc082088cc032489093bcffe8dad3a
gscsdlz <lz842063523@foxmail.com>
2018-04-14 13:12:49 +0800

    完成目标大学的查询部分

---
+++ public/js/school_view.js
@@ -0,0 +1,170 @@
+var attr_num;
+var level;
+
+$(document).ready(function(){
+    $(".sxp-footer").removeClass("navbar-fixed-bottom");
+
+    $.get( URL + "/school/get", function(response){
+        $('.loader-box').remove();
+        if(response.status == true) {
+            $("#schoolInfo").multiLineTable({
+                data : response.data,
+                length : 10,
+                wheelLength : 3,
+                dataHandle : function(d) {
+                    var str = '<tr>' +
+                        '<td style="cursor: pointer">'+d.name+'</td>' +
+                        '<td>'+(d.ranking_1 == null ? '-' : d.ranking_1)+'</td>' +
+                        '<td>'+(d.ranking_3 == null ? '-' : d.ranking_3)+'</td>' +
+                        '<td>'+d.province + '-' + d.city +'</td>' +
+                        '<td>'+d.level +'</td>' +
+                        '<td>'+d.public + '</td>' +
+                        '<td>'+((d.attr_num & (1 << 7)) ? '一流学科' : ((d.attr_num & (1 << 8)) ? '一流大学' : ''))+'</td>' +
+                        ((d.attr_num & (1 << 6)) ? '<td class="ok-gly"><span class="glyphicon glyphicon-ok"></span></td>' : '<td></td>') +
+                        ((d.attr_num & (1 << 5)) ? '<td class="ok-gly"><span class="glyphicon glyphicon-ok"></span></td>' : '<td></td>') +
+                        ((d.attr_num & (1 << 4)) ? '<td class="ok-gly"><span class="glyphicon glyphicon-ok"></span></td>' : '<td></td>') +
+                        ((d.attr_num & (1 << 3)) ? '<td class="ok-gly"><span class="glyphicon glyphicon-ok"></span></td>' : '<td></td>') +
+                        ((d.attr_num & (1 << 2)) ? '<td class="ok-gly"><span class="glyphicon glyphicon-ok"></span></td>' : '<td></td>') +
+                        ((d.attr_num & (1 << 1)) ? '<td class="ok-gly"><span class="glyphicon glyphicon-ok"></span></td>' : '<td></td>') +
+                        ((d.attr_num & (1 << 0)) ? '<td class="ok-gly"><span class="glyphicon glyphicon-ok"></span></td>' : '<td></td>') +
+                        '</tr>';
+                    return str;
+                },
+                userHandle : {
+                    /**
+                     * 搜索按名称学校
+                     * @param originalData
+                     * @returns {Array|any[]}
+                     */
+                    'searchSchool': function (originalData) {
+                        var name = $("#schoolName").val();
+                        var data = [];
+                        for (var i = 0; i < originalData.length; i++) {
+                            if (originalData[i].name.indexOf(name) !== -1) {
+                                data.push(originalData[i])
+                            }
+                        }
+                        return data;
+                    },
+                    /**
+                     * 筛选省份
+                     * @param originalData
+                     */
+                    'fetchProvince': function (originalData) {
+                        var pro = [];
+                        var data = [];
+                        $("#provinceFetch").find(".fetchbox-active").each(function () {
+                            if ($(this).html().indexOf("gly") === -1)
+                                pro.push($(this).html());
+                        })
+                        if (pro.length !== 0) {
+                            data = [];
+                            for (var i = 0; i < originalData.length; i++) {
+                                for (var j = 0; j < pro.length; j++)
+                                    if (originalData[i].province.indexOf(pro[j]) !== -1) {
+                                        data.push(originalData[i]);
+                                        break;
+                                    }
+                            }
+                        } else {
+                            data = originalData;
+                        }
+                        $(".modal-background").hide();
+
+                        return data;
+                    },
+
+                    /**
+                     * 筛选一些属性
+                     */
+                    'fetchAttr' : function (originalData) {
+                        var data = [];
+                        for (var i = 0; i <originalData.length; i++) {
+                            if(originalData[i].attr_num & attr_num) {
+                                data.push(originalData[i]);
+                            } else if(level == 1 && originalData[i].level == '教育部') {
+                                data.push(originalData[i]);
+                            }
+                        }
+                        if(attr_num == 0 && level == 0)
+                            return originalData;
+                        return data;
+                    }
+                },
+            })
+        } else {
+
+        }
+    })
+
+    $("#searchSchool").click(function(){
+        window.dispatchEvent(new Event('searchSchool'));
+    })
+
+    $("#schoolName").keyup(function () {
+        window.dispatchEvent(new Event('searchSchool'));
+    })
+
+    /**
+     * 优化体验，点击搜索框时，默认选中
+     */
+    $("#schoolName").focus(function () {
+        $(this).select();
+    })
+
+
+    /**
+     * 省份框点击时 赋予选中效果
+     */
+    $("#provinceFetch").on("click", "td", function(){
+        if($(this).hasClass('fetchbox-active'))
+            $(this).removeClass('fetchbox-active');
+        else
+            $(this).addClass('fetchbox-active');
+        return false;
+    })
+
+    /**
+     * 启动筛选省份的模态框
+     */
+    $(".glyphicon-th-list").click(function(){
+        $("#provinceList").children(".border-shadow").css('position', 'fixed');
+        $("#provinceList").children(".border-shadow").css('top', $(this).offset().top - document.documentElement.scrollTop+ 20);
+        $("#provinceList").children(".border-shadow").css('left', $(this).offset().left - 300);
+        $("#provinceList").fadeIn();
+    })
+    /**
+     * 隐藏自定义modal
+     */
+    $(".modal-background").click(function () {
+        $(this).fadeOut();
+    })
+    /**
+     * 开始筛选省份
+     */
+    $("#fetch").click(function(){
+        window.dispatchEvent( new Event('fetchProvince'));
+    })
+
+    $(".custom-checkbox").click(function(){
+        var selected = [0,0,0,0];
+        $(".custom-checkbox").each(function(index){
+            if($(this).css('background-position') === '-10px -218px')
+                selected[index] = 1;
+        })
+        attr_num = 0;
+        if(selected[0] === 1) {//双一流
+            attr_num |= 3 << 7; //1 1000 0000
+        }
+        if(selected[1] === 1) { //985
+            attr_num |= 1 << 6; //0 0100 0000
+        }
+        if(selected[2] === 1) {
+            attr_num |= 1 << 5; //0 0010 0000
+        }
+        level = selected[3];
+
+        window.dispatchEvent(new Event("fetchAttr"));
+    })
+
+})
---
+++ resources/views/function/school_view.blade.php
@@ -0,0 +1,124 @@
+@extends('layout')
+@section('title')
+目标大学查询
+@endsection
+@section('navbar')
+    <li class="navbar-a-underline navbar-a-active"><a href="javascript:;">目&nbsp;&nbsp;&nbsp;标&nbsp;&nbsp;&nbsp;大&nbsp;&nbsp;&nbsp;学&nbsp;&nbsp;&nbsp;查&nbsp;&nbsp;&nbsp;询</a></li>
+@endsection
+@section('main')
+<div class="row">
+    <div class="col-md-10 col-md-offset-1 text-center border-shadow" style="padding: 20px">
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label class="control-label col-sm-2">院校名称</label>
+                        <div class="col-sm-8">
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="schoolName" placeholder="输入学校名称搜索">
+                                <div class="input-group-addon"  id="searchSchool"><span class="glyphicon glyphicon-search"></span></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2">院校性质</label>
+                        <div class="col-sm-5 form-control-static">
+                            <span class="custom-checkbox" style="padding-left:30px;background:url({{ URL::asset('images/project/checkbox.gif') }}) no-repeat -10px -18px;"><b>双一流</b></span>
+                            <span class="custom-checkbox" style="padding-left:30px;background:url({{ URL::asset('images/project/checkbox.gif') }}) no-repeat -10px -18px;"><b>985</b></span>
+                            <span class="custom-checkbox" style="padding-left:30px;background:url({{ URL::asset('images/project/checkbox.gif') }}) no-repeat -10px -18px;"><b>211</b></span>
+                            <span class="custom-checkbox" style="padding-left:30px;background:url({{ URL::asset('images/project/checkbox.gif') }}) no-repeat -10px -18px;"><b>教育部直属</b></span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <hr/>
+        <table id="schoolInfo" class="table table-bordered table-striped" >
+            <tr>
+                <th colspan="9">点击 “大学校名” 查看院校详细信息</th>
+                <th colspan="5" style="text-align: center">卓越人才培养计划</th>
+            </tr>
+            <tr>
+                <th style="width: 300px">院校名称</th>
+                <th>全国排名</th>
+                <th>专业排名</th>
+                <th>所在地&nbsp;&nbsp;&nbsp;&nbsp;<span style="cursor: pointer" class="glyphicon glyphicon-th-list"></span></th>
+                <th>高校隶属</th>
+                <th>性质</th>
+                <th>双一流高校</th>
+                <th>985大学</th>
+                <th>211大学</th>
+                <th>工程师</th>
+                <th>医生</th>
+                <th>法律</th>
+                <th>教师</th>
+                <th>农林</th>
+            </tr>
+        </table>
+        <div class="loader-box">
+            <div class="loader">
+            </div>
+            <p>加载中，请稍候！</p>
+        </div>
+    </div>
+</div>
+@endsection
+@section('modal')
+    <div class="modal-background" id="provinceList">
+        <div style="width:600px;padding: 20px;" class="border-shadow">
+            <table class="table text-center table-bordered fetchbox" id="provinceFetch">
+                <tr>
+                    <td>四川</td>
+                    <td>北京</td>
+                    <td>上海</td>
+                    <td>重庆</td>
+                    <td>天津</td>
+                    <td>福建</td>
+                    <td>黑龙江</td>
+                </tr>
+                <tr>
+                    <td>吉林</td>
+                    <td>辽宁</td>
+                    <td>山东</td>
+                    <td>山西</td>
+                    <td>陕西</td>
+                    <td>河北</td>
+                    <td>河南</td>
+                </tr>
+                <tr>
+                    <td>湖北</td>
+                    <td>湖南</td>
+                    <td>江苏</td>
+                    <td>江西</td>
+                    <td>广东</td>
+                    <td>广西</td>
+                    <td>云南</td>
+                </tr>
+                <tr>
+                    <td>海南</td>
+                    <td>贵州</td>
+                    <td>西藏</td>
+                    <td>宁夏</td>
+                    <td>甘肃</td>
+                    <td>青海</td>
+                    <td>内蒙古</td>
+                </tr>
+                <tr>
+                    <td>新疆</td>
+                    <td>安徽</td>
+                    <td>浙江</td>
+                    <td>香港</td>
+                    <td>澳门</td>
+                    <td>台湾</td>
+                    <td style="background-color: white; color:rgb(78,201,142);cursor: pointer" id="fetch">
+                        <span  class="glyphicon glyphicon-search"></span>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+@endsection
+@section('script')
+<script src="{{ URL::asset('js/school_view.js') }}"></script>
+@endsection
